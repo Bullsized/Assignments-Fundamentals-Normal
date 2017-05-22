b254f2fbbd2458df82047d5d80760fc1840831e9
Bullsized <ivanstefanovbg@gmail.com>
2017-05-22 17:42:05 +0300

    C# Intro and Basic Syntax

---
+++ 01 - 04 Course Overview/2017-05-22/02 Add 2 Nums/02 Add 2 Nums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Add_2_Nums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{firstNum} + {secondNum} = {firstNum + secondNum}");
+        }
+    }
+}
---
+++ 01 - 04 Course Overview/2017-05-22/03 Employee Data/03 Employee Data.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Employee_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int employeeID = int.Parse(Console.ReadLine());
+            decimal salary = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}" + Environment.NewLine +
+                $"Age: {age}" + Environment.NewLine +
+                $"Employee ID: {employeeID:D8}" + Environment.NewLine + 
+                $"Salary: {salary:f2}");
+        }
+    }
+}
