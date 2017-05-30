cc853f530e134f768a5248b603a6d50a35415d0c
Bullsized <ivanstefanovbg@gmail.com>
2017-05-30 13:32:50 +0300

    Data Types

---
+++ 10 - 12 Data Types and Var/2017-05-30/02 Circle Area/02 Circle Area.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Circle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal radius = decimal.Parse(Console.ReadLine());
+            Console.WriteLine($"{((decimal)Math.PI * radius * radius):f12}");
+        }
+    }
+}
