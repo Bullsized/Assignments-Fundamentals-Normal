4f5202ea86877f5e861fc5a22a0fdae3e9283e6b
Bullsized <ivanstefanovbg@gmail.com>
2017-05-30 13:43:45 +0300

    Data Types

---
+++ 10 - 12 Data Types and Var/2017-05-30/04 Elevator/04 Elevator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double people = double.Parse(Console.ReadLine());
+            double capacity = double.Parse(Console.ReadLine());
+
+            double carry = Math.Ceiling(people / capacity);
+            Console.WriteLine($"{carry:f0}");
+        }
+    }
+}
