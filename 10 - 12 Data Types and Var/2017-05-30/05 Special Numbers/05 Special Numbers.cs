bfedab2eaf0f1f2020cec4ab59bc7568a652ccc7
Bullsized <ivanstefanovbg@gmail.com>
2017-05-30 13:46:48 +0300

    Data Types

---
+++ 10 - 12 Data Types and Var/2017-05-30/05 Special Numbers/05 Special Numbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int cycle = 1; cycle <= n; cycle++)
+            {
+                int newCycle = cycle;
+                while (newCycle != 0)
+                {
+                    sum += newCycle % 10;
+                    newCycle /= 10;
+                }
+
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    Console.WriteLine($"{cycle} - > True");
+                }
+                else
+                {
+                    Console.WriteLine($"{cycle} - > False");
+                }
+                sum = 0;
+            }
+        }
+    }
+}
---
+++ 10 - 12 Data Types and Var/2017-05-30/06 Triples of Latin L/06 Triples of Latin L.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Triples_of_Latin_L
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
