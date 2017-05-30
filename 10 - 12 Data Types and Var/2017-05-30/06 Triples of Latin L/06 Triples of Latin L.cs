55d1d159ca93466a8c156dbea4a2d023ead23f0c
Bullsized <ivanstefanovbg@gmail.com>
2017-05-30 13:58:10 +0300

    Data Types

--- 10 - 12 Data Types and Var/2017-05-30/06 Triples of Latin L/06 Triples of Latin L.cs
+++ 10 - 12 Data Types and Var/2017-05-30/06 Triples of Latin L/06 Triples of Latin L.cs
@@ -10,6 +10,20 @@ namespace _06_Triples_of_Latin_L
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+
+            char letter = (char)('^' + n);
+
+            for (char first = letter; first < letter + 3; first++)
+            {
+                for (char second = letter; second < letter + 3; second++)
+                {
+                    for (char third = letter; third < letter + 3; third++)
+                    {
+                        Console.WriteLine($"{first}{second}{third}");
+                    }
+                }
+            }
         }
     }
 }
---
+++ 10 - 12 Data Types and Var/2017-05-30/07 Greeting/07 Greeting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string secondName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Hello, {firstName} {secondName}. You are {age} years old.");
+        }
+    }
+}
