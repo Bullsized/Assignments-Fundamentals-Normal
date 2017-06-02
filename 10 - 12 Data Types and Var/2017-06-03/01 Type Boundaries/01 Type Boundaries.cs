4250b60178202f3a2adbe07650c526ba190224bb
Bullsized <ivanstefanovbg@gmail.com>
2017-06-03 00:56:07 +0300

    Data Types

---
+++ 10 - 12 Data Types and Var/2017-06-03/01 Type Boundaries/01 Type Boundaries.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Type_Boundaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "int": Console.WriteLine(int.MaxValue + Environment.NewLine + int.MinValue); break;
+                case "uint": Console.WriteLine(uint.MaxValue + Environment.NewLine + uint.MinValue); break;
+                case "long": Console.WriteLine(long.MaxValue + Environment.NewLine + long.MinValue); break;
+                case "byte": Console.WriteLine(byte.MaxValue + Environment.NewLine + byte.MinValue); break;
+                case "sbyte": Console.WriteLine(sbyte.MaxValue + Environment.NewLine + sbyte.MinValue); break;
+            }
+        }
+    }
+}
