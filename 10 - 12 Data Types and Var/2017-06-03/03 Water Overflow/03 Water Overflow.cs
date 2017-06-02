069e166a81db977369f6330f5caa23ed4550040b
Bullsized <ivanstefanovbg@gmail.com>
2017-06-03 01:01:08 +0300

    Data Types

---
+++ 10 - 12 Data Types and Var/2017-06-03/03 Water Overflow/03 Water Overflow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 10 - 12 Data Types and Var/2017-06-03/06 Catch the Thief/06 Catch the Thief.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Catch_the_Thief
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeOfData = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            long parsing = 0;
+            long storedParse = long.MinValue;
+
+            for (int cycle = 0; cycle < n; cycle++)
+            {
+                parsing = long.Parse(Console.ReadLine());
+
+                if (typeOfData == "sbyte")
+                {
+                    if (sbyte.MinValue <= parsing && parsing <= sbyte.MaxValue)
+                    {
+                        if (parsing > storedParse)
+                        {
+                            storedParse = parsing;
+                        }
+                    }
+                }
+                else if (typeOfData == "int")
+                {
+                    if (int.MinValue <= parsing && parsing <= int.MaxValue)
+                    {
+                        if (parsing > storedParse)
+                        {
+                            storedParse = parsing;
+                        }
+                    }
+                }
+                else if (typeOfData == "long")
+                {
+                    if (long.MinValue <= parsing && parsing <= long.MaxValue)
+                    {
+                        if (parsing > storedParse)
+                        {
+                            storedParse = parsing;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(storedParse);
+        }
+    }
+}
