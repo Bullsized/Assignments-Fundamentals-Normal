5890af1f767887ae147c7ecfcd331478f62b0967
Bullsized <ivanstefanovbg@gmail.com>
2017-06-07 17:37:44 +0300

    Arrays

---
+++ 15 - 16 Arrays/2017-06-07/04 Triple Sum/04 Triple Sum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Triple_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] answer = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            bool lamp = false;
+
+            for (int arrayIndexer = 0; arrayIndexer < answer.Length; arrayIndexer++)
+            {
+                for (int checkDigit = arrayIndexer + 1; checkDigit < answer.Length; checkDigit++)
+                {
+                    int sumOfTwoDigits = answer[arrayIndexer] + answer[checkDigit];
+                    if (answer.Contains(sumOfTwoDigits))
+                    {
+                        Console.WriteLine($"{answer[arrayIndexer]} + {answer[checkDigit]} == {sumOfTwoDigits}");
+                        lamp = true;
+                    }
+                }
+
+            }
+
+            if (lamp == false)
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
---
+++ 15 - 16 Arrays/2017-06-07/05 Rounding Nums/05 Rounding Nums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Rounding_Nums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
