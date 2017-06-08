5890430f146a6339a571e21b2e471d2d768ba540
Bullsized <ivanstefanovbg@gmail.com>
2017-06-09 02:24:39 +0300

    Arrays

---
+++ 15 - 16 Arrays/2017-06-08/05 Compare Char Arrays 2/05 Compare Char Arrays 2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Compare_Char_Arrays_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] firstLine = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
+
+            char[] secondLine = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
+
+            string firstWord = string.Join("", firstLine);
+            string secondWord = string.Join("", secondLine);
+
+            int result = string.Compare(firstWord, secondWord);
+
+            if (result <= 0)
+            {
+                Console.WriteLine(firstWord);
+                Console.WriteLine(secondWord);
+            }
+            else
+            {
+                Console.WriteLine(secondWord);
+                Console.WriteLine(firstWord);
+            }
+        }
+    }
+}
---
+++ 15 - 16 Arrays/2017-06-08/05 Compare Char Arrays/05 Compare Char Arrays.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Compare_Char_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] firstLine = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
+            char[] secondLine = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
+
+            if (firstLine.Length > secondLine.Length)
+            {
+                Console.WriteLine(string.Join("", secondLine));
+                Console.WriteLine(string.Join("", firstLine));
+
+            }
+            else if (firstLine.Length < secondLine.Length)
+            {
+                Console.WriteLine(string.Join("", firstLine));
+                Console.WriteLine(string.Join("", secondLine));
+            }
+            else if (firstLine.Length == secondLine.Length)
+            {
+                for (int i = 0; i < firstLine.Length; i++)
+                {
+                    if (firstLine[i] > secondLine[i])
+                    {
+                        Console.WriteLine(string.Join("", secondLine));
+                        Console.WriteLine(string.Join("", firstLine));
+                        break;
+                    }
+                    if (secondLine[i] > firstLine[i])
+                    {
+                        Console.WriteLine(string.Join("", firstLine));
+                        Console.WriteLine(string.Join("", secondLine));
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
