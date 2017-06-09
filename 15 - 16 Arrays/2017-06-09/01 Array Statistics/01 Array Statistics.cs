362e4507692aa6b9196135130938cfb5681cf465
Bullsized <ivanstefanovbg@gmail.com>
2017-06-09 20:10:17 +0300

    Arrays

---
+++ 15 - 16 Arrays/2017-06-09/01 Array Statistics/01 Array Statistics.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Array_Statistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] listOfDigits = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Console.WriteLine($"Min = {listOfDigits.Min()}");
+            Console.WriteLine($"Max = {listOfDigits.Max()}");
+            Console.WriteLine($"Sum = {listOfDigits.Sum()}");
+            Console.WriteLine($"Average = {listOfDigits.Average()}");
+        }
+    }
+}
---
+++ 15 - 16 Arrays/2017-06-09/02 Мanipulate Array/02 Мanipulate Array.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Мanipulate_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lineOfStrings = Console.ReadLine().Split();
+            int rotation = int.Parse(Console.ReadLine());
+            string[] copyLineOfStrings = new string[lineOfStrings.Length];
+
+            for (int cycle = 0; cycle < rotation; cycle++)
+            {
+                string command = Console.ReadLine();
+
+                bool lamp = false;
+
+                if (command == "Reverse")
+                {
+                    copyLineOfStrings = lineOfStrings.Reverse().ToArray();
+                }
+                else if (command == "Distinct")
+                {
+                    copyLineOfStrings = lineOfStrings.Distinct().ToArray();
+                }
+                else
+                {
+                    string[] listOfCommands = command.Split(' ').ToArray();
+                    int indexToReplace = int.Parse(listOfCommands[1]);
+                    lineOfStrings[indexToReplace] = listOfCommands[2];
+                    lamp = true;
+                }
+
+                if (copyLineOfStrings.Length < lineOfStrings.Length)
+                {
+                    Array.Resize(ref lineOfStrings, copyLineOfStrings.Length);
+                }
+
+                if (lamp != true)
+                {
+                    Array.Copy(copyLineOfStrings, lineOfStrings, copyLineOfStrings.Length);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", lineOfStrings));
+        }
+    }
+}
