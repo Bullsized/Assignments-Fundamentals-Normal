af26a223dcaac609ec57d1b092c03f3653a575a6
Bullsized <ivanstefanovbg@gmail.com>
2017-06-09 20:27:31 +0300

    Arrays

---
+++ 15 - 16 Arrays/2017-06-09/09 Jump Around/09 Jump Around.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _09_Jump_Around
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lineOfDigits = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int sum = lineOfDigits[0];
+
+            for (int cycle = 0; cycle < lineOfDigits.Length; cycle++)
+            {
+                bool possibleTransfer = false;
+
+                if (cycle + lineOfDigits[cycle] < lineOfDigits.Length)
+                {
+                    sum += lineOfDigits[cycle + lineOfDigits[cycle]];
+                    cycle = cycle + lineOfDigits[cycle];
+                    possibleTransfer = true;
+                }
+                else if (0 < cycle - lineOfDigits[cycle])
+                {
+                    sum += lineOfDigits[cycle - lineOfDigits[cycle]];
+                    cycle = cycle - lineOfDigits[cycle];
+                    possibleTransfer = true;
+                }
+
+                if (possibleTransfer == false)
+                {
+                    Console.WriteLine(sum);
+                    break;
+                }
+
+                cycle--;
+            }
+        }
+    }
+}
