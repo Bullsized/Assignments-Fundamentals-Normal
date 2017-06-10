82c6c242a2fdc1b117a36a6c840a6074b72b34ef
Bullsized <ivanstefanovbg@gmail.com>
2017-06-10 21:24:39 +0300

    Arrays

--- 15 - 16 Arrays/2017-06-09/04 Grab and Go/04 Grab and Go.cs
+++ 15 - 16 Arrays/2017-06-09/04 Grab and Go/04 Grab and Go.cs
@@ -10,6 +10,32 @@ namespace _04_Grab_and_Go
     {
         static void Main(string[] args)
         {
+            int[] lineOfDigits = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int searchNum = int.Parse(Console.ReadLine());
+            long sum = 0;
+
+            List<int> putTheIndexes = new List<int>();
+            for (int cycle = 0; cycle < lineOfDigits.Length; cycle++)
+            {
+                if (lineOfDigits[cycle] == searchNum)
+                {
+                    putTheIndexes.Add(cycle);
+                }
+            }
+            if (putTheIndexes.Count > 0)
+            {
+                for (int cycle2 = 0; cycle2 < putTheIndexes[putTheIndexes.Count - 1]; cycle2++)
+                {
+                    sum += lineOfDigits[cycle2];
+                }
+
+                Console.WriteLine(sum);
+
+            }
+            else
+            {
+                Console.WriteLine("No occurrences were found!");
+            }          
         }
     }
 }
