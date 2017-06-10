dc9771d94f9fc73da55c10b17a4a7967ddb250c7
Bullsized <ivanstefanovbg@gmail.com>
2017-06-10 22:31:46 +0300

    Arrays

--- 15 - 16 Arrays/2017-06-09/07 Inv Matcher/07 Inv Matcher.cs
+++ 15 - 16 Arrays/2017-06-09/07 Inv Matcher/07 Inv Matcher.cs
@@ -10,6 +10,25 @@ namespace _07_Inv_Matcher
     {
         static void Main(string[] args)
         {
+            string[] names = Console.ReadLine().Split();
+            long[] quantity = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            decimal[] price = Console.ReadLine().Split().Select(decimal.Parse).ToArray();
+
+            string inputProduct = Console.ReadLine();
+
+            while (inputProduct != "done")
+            {
+                for (int cycle1 = 0; cycle1 < names.Length; cycle1++)
+                {
+                    if (inputProduct == names[cycle1])
+                    {
+                        Console.WriteLine($"{names[cycle1]} costs: {price[cycle1]}; Available quantity: {quantity[cycle1]}");
+                        break;
+                    }
+                }
+
+                inputProduct = Console.ReadLine();
+            }
         }
     }
 }
---
+++ 15 - 16 Arrays/2017-06-09/08 Upgraded Matcher/08 Upgraded Matcher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Upgraded_Matcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+            long[] quantity = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            //If in the quantities array there is no index, which 
+            //corresponds to the name, you should assume the quantity is 0.
+            decimal[] price = Console.ReadLine().Split().Select(decimal.Parse).ToArray();
+
+            string[] inputName = Console.ReadLine().Split();
+
+            while (inputName[0] != "done")
+            {
+                long qtityToOrder = long.Parse(inputName[1]);
+
+                for (int cycle = 0; cycle < names.Length; cycle++)
+                {
+                    if (inputName[0] == names[cycle])
+                    {
+                        if (cycle < quantity.Length && quantity[cycle] >= qtityToOrder)
+                        {
+                            Console.WriteLine($"{names[cycle]} x {qtityToOrder} costs {qtityToOrder * price[cycle]:f2}");
+                            quantity[cycle] -= qtityToOrder;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"We do not have enough {names[cycle]}");
+                        }
+                    }
+                }
+
+                inputName = Console.ReadLine().Split();
+            }
+        }
+    }
+}
