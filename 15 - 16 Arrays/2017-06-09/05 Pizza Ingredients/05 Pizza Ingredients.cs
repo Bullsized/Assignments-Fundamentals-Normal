ea20b6a59f316aed7e1117ed1a9069b2e06e6fbf
Bullsized <ivanstefanovbg@gmail.com>
2017-06-10 22:06:41 +0300

    Arrays

---
+++ 15 - 16 Arrays/2017-06-09/05 Pizza Ingredients/05 Pizza Ingredients.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Pizza_Ingredients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ingredients = Console.ReadLine().Split();
+            int specificLength = int.Parse(Console.ReadLine());
+
+            string[] nameOfIngredients = new string[10];
+
+            int nextIngredient = 0;
+
+            int countOfIngredients = 0;
+
+            for (int cycle1 = 0; cycle1 < ingredients.Length; cycle1++)
+            {
+                if (ingredients[cycle1].Length == specificLength)
+                {
+                    countOfIngredients++;
+                    Console.WriteLine($"Adding {ingredients[cycle1]}.");
+                    nameOfIngredients[nextIngredient] = ingredients[cycle1];
+                    nextIngredient++;
+                }
+
+                if (countOfIngredients == 10)
+                {
+                    break;
+                }
+            }
+
+            PrintThePizzas(nameOfIngredients, countOfIngredients);
+
+        }
+
+        private static void PrintThePizzas(string[] nameOfIngredients, int countOfIngredients)
+        {
+            Array.Resize(ref nameOfIngredients, countOfIngredients);
+            Console.WriteLine($"Made pizza with total of {countOfIngredients} ingredients.");
+            Console.WriteLine($"The ingredients are: {string.Join(", ", nameOfIngredients)}.");
+        }
+    }
+}
---
+++ 15 - 16 Arrays/2017-06-09/06 Heists/06 Heists.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Heists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lineOfDigits = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int priceOfJewels = lineOfDigits[0];
+            int priceOfGold = lineOfDigits[1];
+
+            string[] lootAndExpenses = Console.ReadLine().Split();
+
+            int totalEarnings = 0;
+            int totalExpenses = 0;
+
+            while (lootAndExpenses[0] != "Jail")
+            {
+                totalExpenses += int.Parse(lootAndExpenses[1]);
+
+                char[] splittedLoot = lootAndExpenses[0].ToCharArray();
+
+                for (int cycle1 = 0; cycle1 < splittedLoot.Length; cycle1++)
+                {
+                    if (splittedLoot[cycle1] == '%')
+                    {
+                        totalEarnings += priceOfJewels;
+                    }
+                    else if (splittedLoot[cycle1] == '$')
+                    {
+                        totalEarnings += priceOfGold;
+                    }
+                }
+
+                lootAndExpenses = Console.ReadLine().Split();
+            }
+
+            if (totalEarnings >= totalExpenses)
+            {
+                Console.WriteLine($"Heists will continue. Total earnings: {totalEarnings - totalExpenses}.");
+            }
+            else
+            {
+                Console.WriteLine($"Have to find another job. Lost: {totalExpenses - totalEarnings}.");
+            }
+        }
+    }
+}
