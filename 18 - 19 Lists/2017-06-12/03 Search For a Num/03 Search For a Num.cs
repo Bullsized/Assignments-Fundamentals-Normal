fb60b58e8dcdfb07e92540d14997d24710bb03c4
Bullsized <ivanstefanovbg@gmail.com>
2017-06-12 23:59:47 +0300

    Lists

---
+++ 18 - 19 Lists/2017-06-12/03 Search For a Num/03 Search For a Num.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Search_For_a_Num
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lineOfDigits = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            List<int> lineOfCommands = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int takeThisMuchDigits = lineOfCommands[0];
+
+            int removeThisMuchDigits = lineOfCommands[1];
+
+            int checkThisOut = lineOfCommands[2];
+
+            lineOfDigits.Take(takeThisMuchDigits);
+            lineOfDigits.RemoveRange(0, removeThisMuchDigits);
+
+            if (lineOfDigits.Contains(checkThisOut))
+            {
+                Console.WriteLine("YES!");
+            }
+            else
+            {
+                Console.WriteLine("NO!");
+            }
+        }
+    }
+}
